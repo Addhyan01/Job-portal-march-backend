a1fdfa677b7e16187efaffe0ae375738054fbe5b
Addhyan01 <addhyankumar@gmail.com>
2025-03-03 17:40:09 +0530

    dotenv update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const dotenv = require('dotenv');
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
@@ -15,7 +16,7 @@ mongoose.connect(process.env.mongoURI, {
   })
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
-    
+
 const app = express();
 app.use(cors());
 
